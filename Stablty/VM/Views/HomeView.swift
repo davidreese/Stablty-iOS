968381b0a7259d0ab0be517e93e8179ee9181d10
David Reese <dir2100@me.com>
2023-08-16 12:23:27 -0400

    home view updated for scrolling

--- Stablty/VM/Views/HomeView.swift
+++ Stablty/VM/Views/HomeView.swift
@@ -24,88 +24,92 @@ struct HomeView: View {
     }
     
     var face: some View {
-        VStack {
-            // intoduction bar
-            Group {
-                RoundedRectangle(cornerRadius: StabltyUI.standardCornerRadius)
-                    .foregroundStyle(RadialGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.0, saturation: 0.0, brightness: 0.9506747693900603, opacity: 1.0), location: 0.0), Gradient.Stop(color: Color(hue: 0.1, saturation: 0.0, brightness: 1.0, opacity: 1.0), location: 1.0)]), center: UnitPoint.topLeading, startRadius: 132.40559895833334, endRadius: 585.3841145833333))
-                    .shadow(color: Color(white: 0.8),radius: 3, x: 2, y: 2)
-                    .overlay {
-                        HStack {
-                            Image(systemName: "questionmark")
-                                .padding(.trailing)
-                            Text("Introduction to Mindfulness")
-                                .font(Font.custom(Fonts.poppinsRegular, size: 16))
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                        .foregroundColor(Color("StabltyPurple"))
-                    }
-                /*
-                 .overlay {
-                 HStack {
-                 Image(systemName: "questionmark")
-                 Spacer()
-                 }
-                 .padding()
-                 .foregroundColor(Color("StabltyPurple"))
-                 }
-                 */
-            }
-            .frame(height: 40)
-            .padding()
-            
-            Group {
-                HStack {
-                    Text("Your Top Picks")
-                        .font(Font.custom(Fonts.poppinsMedium, size: 19))
-                        .foregroundColor(Color("StabltyPurple"))
-                    Spacer()
-                }.padding(.horizontal)
-                
-            }
-            
-            Spacer()
-            
+        ScrollView(.vertical, showsIndicators: false) {
             VStack {
-                HStack {
-                    Text("Recently Accessed")
-                        .font(Font.custom(Fonts.poppinsMedium, size: 19))
-                        .foregroundColor(Color("StabltyPurple"))
-                    Spacer()
-                }.padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        Spacer()
-                            .frame(width: 16)
-                        ForEach(1..<5) { _ in
-                            ImageAndTextButton(title: "Title here", description: "Descripion goes here")
-                                .frame(width: 175, height: 100)
+                // intoduction bar
+                Group {
+                    RoundedRectangle(cornerRadius: StabltyUI.standardCornerRadius)
+                        .foregroundStyle(RadialGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.0, saturation: 0.0, brightness: 0.9506747693900603, opacity: 1.0), location: 0.0), Gradient.Stop(color: Color(hue: 0.1, saturation: 0.0, brightness: 1.0, opacity: 1.0), location: 1.0)]), center: UnitPoint.topLeading, startRadius: 132.40559895833334, endRadius: 585.3841145833333))
+                        .shadow(color: Color(white: 0.8),radius: 3, x: 2, y: 2)
+                        .overlay {
+                            HStack {
+                                Image("StabltyHeartPlant")
+                                    .resizable()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .padding([.trailing, .vertical],5)
+                                Text("Introduction to Mindfulness")
+                                    .font(Font.custom(Fonts.poppinsRegular, size: 16))
+                                Spacer()
+                            }
+                            .padding(.horizontal)
+                            .foregroundColor(Color("StabltyPurple"))
                         }
+                    /*
+                     .overlay {
+                     HStack {
+                     Image(systemName: "questionmark")
+                     Spacer()
+                     }
+                     .padding()
+                     .foregroundColor(Color("StabltyPurple"))
+                     }
+                     */
+                }
+                .frame(height: 40)
+                .padding()
+                
+                Group {
+                    HStack {
+                        Text("Your Top Picks")
+                            .font(Font.custom(Fonts.poppinsMedium, size: 19))
+                            .foregroundColor(Color("StabltyPurple"))
                         Spacer()
-                            .frame(width: 16)
+                    }.padding(.horizontal)
+                    
+                }
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text("Recently Accessed")
+                            .font(Font.custom(Fonts.poppinsMedium, size: 19))
+                            .foregroundColor(Color("StabltyPurple"))
+                        Spacer()
+                    }.padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            Spacer()
+                                .frame(width: 16)
+                            ForEach(1..<5) { _ in
+                                ImageAndTextButton(title: "Title here", description: "Descripion goes here")
+                                    .frame(width: 175, height: 100)
+                            }
+                            Spacer()
+                                .frame(width: 16)
+                        }
                     }
                 }
+                
+                Spacer()
             }
-            
-            Spacer()
-        }
-        .navigationTitle("Home")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Image("StabltyHead")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32, height: 32)
-                //                        .background(.red)
-                    .aspectRatio(contentMode: .fit)
-            }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "line.3.horizontal")
+            .navigationTitle("Home")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("StabltyHead")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                    //                        .background(.red)
+                        .aspectRatio(contentMode: .fit)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
                 }
             }
         }
