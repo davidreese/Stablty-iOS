d75e365ab422207f90e1eb3e682ff0f1988a0a22
David Reese <dir2100@me.com>
2023-08-16 15:43:45 -0400

    added progressview to login

--- Stablty/VM/Views/LoginView.swift
+++ Stablty/VM/Views/LoginView.swift
@@ -14,75 +14,88 @@ struct LoginView: View {
     @AppStorage("token") var token: String?
     @AppStorage("rawNonce") var rawNonce: String?
     
+    @State var waiting = false
+    
     var body: some View {
-        SignInWithAppleButton(.continue) { authRequest in
-            authRequest.requestedScopes = [.fullName, .email]
-            let rawNonce = randomNonceString()
-            authRequest.nonce = sha256(rawNonce)
-            self.rawNonce = rawNonce
-        } onCompletion: { result in
-            switch result {
+        VStack {
+            SignInWithAppleButton(.continue) { authRequest in
+                authRequest.requestedScopes = [.fullName, .email]
+                let rawNonce = randomNonceString()
+                authRequest.nonce = sha256(rawNonce)
+                self.rawNonce = rawNonce
+            } onCompletion: { result in
+                waiting = true
+                switch result {
                 case .success(let authorization):
-                
-                guard let nonce = rawNonce else {
-                  fatalError("Invalid state: A login callback was received, but no login request was sent.")
-                }
-                
-                guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential, let identityToken = appleIDCredential.identityToken else {
-                    print("Unable to fetch data")
-                    return
-                }
-                
-//                let userId = appleIDCredential.user
-//                let authCode = appleIDCredential.authorizationCode
-//                let email = appleIDCredential.email
-//                let givenName = appleIDCredential.fullName?.givenName
-//                let familyName = appleIDCredential.fullName?.familyName
-                print("Real User Status: \(appleIDCredential.realUserStatus)")
-
-                
-//                Leaving out state unless that will be implemented
-//                    let state = appleIDCredential.state
-                
-                guard let idTokenString = String(data: identityToken, encoding: .utf8) else {
-                  print("Unable to serialize token string from data: \(identityToken.debugDescription)")
-                  return
-                }
-                
-                guard verify(appleIdToken: idTokenString) else {
-                    return
-                }
-                
-                
-                
-                /*
-                 Note from the dev. license agreement:
-                 If a Sign in with Apple user has chosen to anonymize their user data as part of Sign In with Apple, You agree not to attempt to link such anonymized data with information that directly identifies the individual and that is obtained outside of Sign In with Apple without first obtaining user consent.
-                 */
-                
-                let credential = OAuthProvider.appleCredential(withIDToken: idTokenString, rawNonce: nonce, fullName: appleIDCredential.fullName)
-                
-                      // Sign in with Firebase.
-                Auth.auth().signIn(with: credential) { (authResult, error) in
+                    
+                    guard let nonce = rawNonce else {
+                        fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                    }
+                    
+                    guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential, let identityToken = appleIDCredential.identityToken else {
+                        print("Unable to fetch data")
+                        return
+                    }
+                    
+                    //                let userId = appleIDCredential.user
+                    //                let authCode = appleIDCredential.authorizationCode
+                    //                let email = appleIDCredential.email
+                    //                let givenName = appleIDCredential.fullName?.givenName
+                    //                let familyName = appleIDCredential.fullName?.familyName
+                    print("Real User Status: \(appleIDCredential.realUserStatus)")
+                    
+                    
+                    //                Leaving out state unless that will be implemented
+                    //                    let state = appleIDCredential.state
+                    
+                    guard let idTokenString = String(data: identityToken, encoding: .utf8) else {
+                        print("Unable to serialize token string from data: \(identityToken.debugDescription)")
+                        return
+                    }
+                    
+                    guard verify(appleIdToken: idTokenString) else {
+                        return
+                    }
+                    
+                    
+                    
+                    /*
+                     Note from the dev. license agreement:
+                     If a Sign in with Apple user has chosen to anonymize their user data as part of Sign In with Apple, You agree not to attempt to link such anonymized data with information that directly identifies the individual and that is obtained outside of Sign In with Apple without first obtaining user consent.
+                     */
+                    
+                    let credential = OAuthProvider.appleCredential(withIDToken: idTokenString, rawNonce: nonce, fullName: appleIDCredential.fullName)
+                    
+                    // Sign in with Firebase.
+                    Auth.auth().signIn(with: credential) { (authResult, error) in
+                        
+                        waiting = false
+                        
                         if let error {
-                          // Error. If error.code == .MissingOrInvalidNonce, make sure
-                          // you're sending the SHA256-hashed nonce as a hex string with
-                          // your request to Apple.
-                          print("Couldn't sign in with Firebase: \(error.localizedDescription)")
-                          return
+                            // Error. If error.code == .MissingOrInvalidNonce, make sure
+                            // you're sending the SHA256-hashed nonce as a hex string with
+                            // your request to Apple.
+                            print("Couldn't sign in with Firebase: \(error.localizedDescription)")
+                            return
                         }
                         // User is signed in to Firebase with Apple.
-                          
-                          self.token = idTokenString
-                      }
-                
+                        
+                        self.token = idTokenString
+                    }
+                    
                     break
                 case .failure(let error):
-                
+                    
                     break
+                }
+            }
+            .scaledToFit()
+            .frame(width: 200)
+            
+            if waiting {
+                ProgressView()
             }
         }
-        .scaledToFit()
     }
     
     /// Returns `true` if the apple id token meets specifications.
@@ -90,9 +103,9 @@ struct LoginView: View {
     func verify(appleIdToken: String) -> Bool {
         let json = decode(jwtToken: appleIdToken)
         /*
-        json
-    https://developer.apple.com/documentation/sign_in_with_apple/sign_in_with_apple_rest_api/verifying_a_user
-    https://developer.apple.com/forums/thread/708415*/
+         json
+         https://developer.apple.com/documentation/sign_in_with_apple/sign_in_with_apple_rest_api/verifying_a_user
+         https://developer.apple.com/forums/thread/708415*/
         
         return true
     }
