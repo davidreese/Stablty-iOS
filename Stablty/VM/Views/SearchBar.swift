59c82f511c8f09b6633e3d1dc83707a5daa32b28
David Reese <dir2100@me.com>
2024-03-29 09:58:42 -0400

    partiall implemented search bar

---
+++ Stablty/VM/Views/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Stablty
+//
+//  Created by David Reese on 3/29/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
